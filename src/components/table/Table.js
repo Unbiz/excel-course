a019731dd6a53c266d29790855b94060a81e8499
Vladimir Bessonov <bessmy@gmail.com>
2021-01-07 20:06:52 +0300

    some changes

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,44 @@
 import {ExcelComponent} from '../../core/ExcelComponent';
 import {createTable} from './table.template';
+import {$} from '../../core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable(20);
+  }
+  
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target);
+      const $parent = $resizer.closest('[data-type="resizable"]');
+      const coords = $parent.getCoords();
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
+      const type = $resizer.data.resize;
+
+
+      document.onmousemove = e => {
+        if (type === 'col') {
+          const delta = e.pageX - coords.right;
+          const value = coords.width + delta;
+          cells.forEach(el => el.style.width = value + 'px');
+        } else {
+          const delta = e.pageY - coords.bottom;
+          const value = coords.height + delta;
+          $parent.$el.style.height = value + 'px';
+        }
+      };
+
+      document.onmouseup = () => {
+        document.onmousemove = null;
+      };
+    }
   }
 }
